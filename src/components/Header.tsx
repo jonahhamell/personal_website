59fc2a991b4eb4a16f030ee1be54b65198456cbb
Jonah Hamell <149976661+jonahhamell@users.noreply.github.com>
2026-05-28 14:42:23 -0500

    [FIX] Fixed Implementation of buttons the create buttons in the center of header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,30 @@ import { Popover, PopoverGroup, PopoverButton } from '@headlessui/react'
 function Header() {
     return (
         <header className="bg-white">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+            <nav className="mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-8">
                 
-                <PopoverGroup className="flex gap-x-12">
+                <PopoverGroup className="flex gap-x-12 items-center">
                     
                     {/*added buttons for the header to be able to navigate throughout the websites, will add functionality at a later date.*/}
-                    <Popover className="relative">
 
-                        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 text-center">
+                    <Popover>
+                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
                             About Me
                         </PopoverButton>
-
-                        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 text-center">
+                    </Popover>
+                        
+                    <Popover>
+                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
                             Projects
                         </PopoverButton>
+                    </Popover>
 
-                        <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-gray-900 text-center">
+                    <Popover>
+                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
                             Contact
                         </PopoverButton>
-
                     </Popover>
+
 
                 </PopoverGroup>
 
