0afc18c58840eb8af7d930be215dd14ec2ad28b3
Jonah Hamell <149976661+jonahhamell@users.noreply.github.com>
2026-05-28 14:57:39 -0500

    [Update] Added Logo to Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { Popover, PopoverGroup, PopoverButton } from '@headlessui/react'
+import signature from '../assets/signature.png'
+
 
 
 function Header() {
     return (
         <header className="bg-white">
-            <nav className="mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-8">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+
+                {/*added a little logo to the header, which was a signature that I had generated for me.*/}
+                <img src={signature} alt="Signature" className="h-13 w-auto mt-2" />
                 
                 <PopoverGroup className="flex gap-x-12 items-center">
                     
                     {/*added buttons for the header to be able to navigate throughout the websites, will add functionality at a later date.*/}
-
                     <Popover>
                         <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
                             About Me
