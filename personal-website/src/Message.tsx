af43924327e1c8bce4badd33d60ea62884340540
Jonah Hamell <149976661+jonahhamell@users.noreply.github.com>
2026-05-28 02:09:07 -0500

    [Configuration] Implemented React+Vite Skeleton Code

---
+++ personal-website/src/Message.tsx
@@ -0,0 +1,11 @@
+function Message() {
+    return (
+        <div>
+            <h1>This Website is Currently a Work in Progress</h1>
+            <p>This is a project being worked on throughout summer 2026, Estimated Time of Completion is around middle of June.</p>
+        </div>
+    );
+}
+
+
+export default Message;
