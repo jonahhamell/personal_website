3e445ae2e647b4f2f79b9c1b518be2bd2046d4dc
Jonah Hamell <149976661+jonahhamell@users.noreply.github.com>
2026-05-29 01:26:42 -0500

    [FIX] Fixed Layout of the Header after implementation of the Switch.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,35 +14,38 @@ function Header() {
 
                 {/*added a little logo to the header, which was a signature that I had generated for me.*/}
                 <img src={signature} alt="Signature" className="h-13 w-auto mt-2 animate-pulse" />
+
+                <div className="flex items-center gap-x-12">
                 
-                <PopoverGroup className="flex gap-x-12 items-center">
-                    
-                    {/*added buttons for the header to be able to navigate throughout the websites, will add functionality at a later date.*/}
-                    <Popover>
-                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
-                            About Me
-                        </PopoverButton>
-                    </Popover>
+                    <PopoverGroup className="flex gap-x-12 items-center">
                         
-                    <Popover>
-                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
-                            Projects
-                        </PopoverButton>
-                    </Popover>
+                        {/*added buttons for the header to be able to navigate throughout the websites, will add functionality at a later date.*/}
+                        <Popover>
+                            <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
+                                About Me
+                            </PopoverButton>
+                        </Popover>
+                            
+                        <Popover>
+                            <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
+                                Projects
+                            </PopoverButton>
+                        </Popover>
 
-                    <Popover>
-                        <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
-                            Contact
-                        </PopoverButton>
-                    </Popover>
+                        <Popover>
+                            <PopoverButton className="text-sm/6 font-semibold text-gray-900 hover:text-teal-500 transition-colors duration-200">
+                                Contact
+                            </PopoverButton>
+                        </Popover>
 
 
-                </PopoverGroup>
+                    </PopoverGroup>
 
-                {/*added a dark mode toggle switch, will add functionality at a later date.*/}
-                <Switch checked = {enabled} onChange = {setEnabled} className= "group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-checked:bg-teal-600">
-                    <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-6" />
-                </Switch>
+                    {/*added a dark mode toggle switch, will add functionality at a later date.*/}
+                    <Switch checked = {enabled} onChange = {setEnabled} className= "group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-checked:bg-teal-600">
+                        <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-6" />
+                    </Switch>
+                </div>
                     
 
             </nav>
